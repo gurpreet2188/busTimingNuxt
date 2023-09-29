60707e26a4d35a52e4914b3281c756c249022a4f
gurpreet88.sandhu@gmail.com <gurpreet88.sandhu@gmail.com>
2023-09-29 08:25:13 +0800

    bus card text size

--- components/card/stopInfo.vue
+++ components/card/stopInfo.vue
@@ -1,48 +1,37 @@
 <script setup lang="ts">
 defineProps({ stopName: String, stopCode: String, streetName: String, stopPos: Object, distanceToStop: Number })
 
-const divRef: Ref<HTMLDivElement | null> = ref(null)
-const titleRef: Ref<HTMLHRElement | null> = ref(null)
-onMounted(() => {
-    if (divRef.value && titleRef.value) {
-        console.log(divRef.value.clientWidth, titleRef.value.clientWidth)
-        if ((titleRef.value.clientWidth+32) >= divRef.value.clientWidth) {
-            const diff = divRef.value.clientWidth / titleRef.value.clientWidth
-            // formula to auto-resize text length for title, 
-            // (defaultFontSize x 16) - ((defaultFontSize * 16) * (1 - difference of text length vs parent div length))
-            // defaultFontSize is 0.1 is less to fit the text within the parent div padding.
-            // different font and font styles (letter spacing) may require different adjustments for defaultFontSize
-            titleRef.value.style.fontSize = ((1.17 * 16) - ((1.17 * 16) * (1 - diff))) + 'px'
-        }
-    }
-})
+// const divRef: Ref<HTMLDivElement | null> = ref(null)
+// const titleRef: Ref<HTMLHRElement | null> = ref(null)
+// onMounted(() => {
+//     if (divRef.value && titleRef.value) {
+//         console.log(divRef.value.clientWidth, titleRef.value.clientWidth)
+//         if ((titleRef.value.clientWidth+32) >= divRef.value.clientWidth) {
+//             const diff = divRef.value.clientWidth / titleRef.value.clientWidth
+//             // formula to auto-resize text length for title, 
+//             // (defaultFontSize x 16) - ((defaultFontSize * 16) * (1 - difference of text length vs parent div length))
+//             // defaultFontSize is 0.1 is less to fit the text within the parent div padding.
+//             // different font and font styles (letter spacing) may require different adjustments for defaultFontSize
+//             titleRef.value.style.fontSize = ((1.17 * 16) - ((1.17 * 16) * (1 - diff))) + 'px'
+//         }
+//     }
+// })
 </script>
 
 <template>
-    <div ref="divRef"
-        class="flex flex-col justify-center items-start gap-[0.5rem] p-[1rem] w-[100%] border-b-[1px] border-black/10 dark:border-[#6d6875]/50 bg-[#ffb4a2] dark:bg-[#1b263b]/0 text-[#b5838d] dark:text-[#ffcdb2]">
-        <div class="flex justify-center items-center gap-[1rem]">
-           
-            <a target="_blank" :href="`https://www.google.com/maps/search/?api=1&query=${stopPos?.lat},${stopPos?.lon}`"
-                class="text-xs bg-[#b5838d] font-light p-2 rounded-md text-[#ffcdb2] shadow-sm border border-[#ffcdb2]/0 dark:border-[#ffcdb2] dark:bg-[#1b263b] flex justify-center items-center gap-[0.5rem]">
-                <h2 class="text-[1.3rem] whitespace-nowrap">
-                {{stopCode }} 
-            </h2> / 
-                <IconsNavigateTo :color="'#ffcdb2'" :size="{w:'12px',h:'12px'}"/>
-                {{ distanceToStop ? distanceToStop + ' km' : 'Go To' }}</a>
-        </div>
-        <!-- <a target="_blank" :href="`https://www.google.com/maps/search/?api=1&query=${stopPos?.lat},${stopPos?.lon}`"
-                class="text-xs font-light p-1 rounded-full border border-[#ffcdb2]/20 flex gap-[0.5rem]">
-                <IconsNavigateTo :color="'#ffcdb2'" :size="{w:'16px',h:'16px'}"/>
-                {{ distanceToStop ? distanceToStop + ' km' : 'Go To' }}</a> -->
-
-        <p class="font-normal text-sm">{{stopName}}</p>
-        
-        <div class="flex justify-between items-center w-[100%]">
-            <div class="w-[50%]">
-                <p class="font-normal text-xs">{{ streetName }}</p>
-            </div>
-            
+    <div
+        class="flex flex-col justify-center items-start gap-[1rem] p-[1rem] w-[100%] border-b-[1px] border-black/10 dark:border-[#6d6875]/50 bg-[#ffb4a2] dark:bg-[#1b263b]/0 text-[#6d6875] dark:text-[#ffcdb2]">
+        <a target="_blank" :href="`https://www.google.com/maps/search/?api=1&query=${stopPos?.lat},${stopPos?.lon}`"
+            class="text-xs bg-[#b5838d] font-light p-2 rounded-md text-[#ffcdb2] shadow-sm border border-[#ffcdb2]/0 dark:border-[#ffcdb2] dark:bg-[#1b263b] flex justify-center items-center gap-[0.5rem]">
+            <h2 class="text-[1.3rem] whitespace-nowrap">
+                {{ stopCode }}
+            </h2> /
+            <IconsNavigateTo :color="'#ffcdb2'" :size="{ w: '12px', h: '12px' }" />
+            {{ distanceToStop ? distanceToStop + ' km' : 'Go To' }}
+        </a>
+        <div class="flex flex-col justify-between items-start w-[100%]">
+            <p class="line-clamp-1">{{ stopName }}</p>
+            <p class="text-sm">{{ streetName }}</p>
         </div>
     </div>
 </template>
