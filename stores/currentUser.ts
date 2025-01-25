b2b3a735f8075a2f2a03a34b4af7a68af836c669
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2025-01-25 21:50:52 +0800

    deps. fix attempt 2

--- stores/currentUser.ts
+++
@@ -1,5 +0,0 @@
-import { defineStore } from "pinia";
-import { useCurrentUser } from 'vuefire';
-export const useCurrentUserStore = defineStore('currentUser', {
-    state: ()=> {return useCurrentUser()}
-})
